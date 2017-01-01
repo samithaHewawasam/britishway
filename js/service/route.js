f5ed8bc7699ab65e8a061e22e627c16366945be7
Samitha Hewawasam ( underscore ) <samithahewawasam@gmail.com>
2017-01-01 13:57:07 +0530

    remove prefix from url

--- js/service/route.js
+++ js/service/route.js
@@ -1,6 +1,7 @@
 app.config(function($routeProvider, $locationProvider) {
 
 //$locationProvider.html5Mode(true);
+$locationProvider.hashPrefix('');
     $routeProvider
         .when('/', {
             title: 'Dashboard',
